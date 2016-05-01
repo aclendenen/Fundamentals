f7206e99f727f75d6303a4dce735f96eab320e6e
Jeffrey Ray Obadal <Jeffrey-Obadal@UIowa.edu>
2016-04-30 19:54:06 -0500

    change updateItem

--- php/class.user.php
+++ php/class.user.php
@@ -578,7 +578,7 @@ class USER
 	{
 		try
 		{
-			$stmt = $this->conn->prepare("UPDATE items SET name = '$itemName', category = '$itemCategory', supplier ='$itemSupplier', color= '$itemColor', dimensions = $itemDimensions, price = $itemPrice, in_stock = $itemStock, lead_time=$leadTime, description= $itemDescription WHERE item_id= $itemId")
+			$stmt = $this->conn->prepare("UPDATE items SET name = '$itemName', category = '$itemCategory', supplier ='$itemSupplier', color= '$itemColor', dimensions = '$itemDimensions', price = $itemPrice, in_stock = $itemStock, lead_time='$leadTime', description= '$itemDescription' WHERE item_id= $itemId")
 			    or die(mysql_error());
 			$stmt->execute();	
 			
--- php/edit_item.php
+++ php/edit_item.php
@@ -44,16 +44,16 @@
 	  {
 	    if( $user->updateItem($itemId,$itemName,$itemCategory,$itemSupplier,$itemColor,$itemDimensions,$itemPrice,$itemStock,$leadTime,$itemDescription) )
 	    {
-	      $user->redirect_with_flash("edit_item.php?itemId=".$itemId,"update Success!");
+	      $user->redirect_with_flash("home.php","update Success!");
 	    }
 	    else
 	    {
-	    	$user->redirect_with_flash("edit_item.php?itemId=".$itemId, "update failure!");
+	    	$user->redirect_with_flash("home.php", "update failure!");
 	    }
 	  }
 	  catch(PDOException $e)
 	  {
-	    $user->redirect_with_flash("edit_item.php?itemId=".$itemId, "Sorry something went wrong!");
+	    $user->redirect_with_flash("home.php", "Sorry something went wrong!");
 	    //echo $e->getMessage();
 	  }
 	}	
